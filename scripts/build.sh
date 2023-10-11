a95225b255efbf282e3cc7377c784cc377c4ff38
Flavio Smirne <smirne@outlook.com>
2023-10-11 09:27:30 -0500

    attempt to fix build on WSL

--- scripts/build.sh
+++ scripts/build.sh
@@ -509,7 +509,8 @@ if [ "$REMOVE_AMAZON" ]; then
 fi
 
 echo "Add device administration features"
-sudo sed -ie '/cts/a \    <feature name="android.software.device_admin" />' -e '/print/i \    <feature name="android.software.managed_users" />' "$VENDOR_MNT/etc/permissions/windows.permissions.xml"
+sudo sed -ie '/cts/a \    <feature name="android.software.device_admin" />' "$VENDOR_MNT/etc/permissions/windows.permissions.xml"
+sudo sed -ie '/picture_in_picture/i \    <feature name="android.software.managed_users" />' "$VENDOR_MNT/etc/permissions/windows.permissions.xml"
 sudo setfattr -n security.selinux -v "u:object_r:vendor_configs_file:s0" "$VENDOR_MNT/etc/permissions/windows.permissions.xml" || abort
 echo -e "done\n"
 
